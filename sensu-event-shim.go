e23cd2e5bb4460211a2beb3addd41b798d0cd449
Andrew Rowson <github@growse.com>
2014-04-12 13:29:12 +0100

    Create sensu-event-shim.go

---
+++ sensu-event-shim.go
@@ -0,0 +1,14 @@
+package "github.com/growse/sensu-event-shim"
+
+import (
+  "database/sql"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+  
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":5151", nil)
+}
