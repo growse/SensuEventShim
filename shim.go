0e6d786d3f79b909cb5ca3c542d667e11f246116
Andrew Rowson <github@growse.com>
2014-04-12 14:07:53 +0100

    initial version that should work

--- shim.go
+++ shim.go
@@ -1,16 +1,49 @@
-package sensueventshim
+package main
 
 import (
-  "database/sql"
-  "net/http"
-  "fmt"
+    _ "github.com/lib/pq"
+    "database/sql"
+    "net/http"
+    "log"
+    "io/ioutil"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func eventhandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "POST" {
+        body, err := ioutil.ReadAll(r.Body)
+        if err != nil {
+            log.Print(err)
+            http.Error(w, "Bad Request", 400)
+            return
+        }
+        _, err = stmt.Exec(body)
+        if err != nil {
+            log.Print(err)
+            http.Error(w, "Bad Request", 400)
+            return
+        }
+    } else {
+        http.Error(w, "Bad Request", 400)
+    }
 }
-  
+
+var db *sql.DB
+var stmt *sql.Stmt
+
 func main() {
-    http.HandleFunc("/", handler)
+    var err error
+    db, err = sql.Open("postgres", "user=sensuevents dbname=sensuevents")
+    if err != nil {
+        log.Fatal(err)
+    }
+    err = db.Ping()
+    if err != nil {
+        log.Fatal(err)
+    }
+    stmt, err = db.Prepare("INSERT INTO events (event) VALUES(?)")
+    if err != nil {
+        log.Fatal(err)
+    }
+    http.HandleFunc("/events", eventhandler)
     http.ListenAndServe(":5151", nil)
 }
